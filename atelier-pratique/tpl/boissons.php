3ffa5a36432fd0d0472559d40c1fe9fbc0d3f6c1
Sou59 <sz5962@gmail.com>
2018-06-19 22:43:50 +0200

    charte

--- atelier-pratique/tpl/boissons.php
+++ atelier-pratique/tpl/boissons.php
@@ -11,16 +11,37 @@
         foreach($items as $item) {
             ?>
 
-            
-                <div class="col-sm-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $item["title"] ?></h5>
-                            <p class="card-text"><?= $item["description"] ?></p>
-                            <a href="#" class="btn btn-primary">Plus d'infos</a>
+
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="row">
+                        <div class="col-6">
+                            <h5 class="card-title pt-2 pl-2"><?= $item["title"] ?></h5>
+                        </div>
+                        <div class="col-6 text-right pt-2">
+                            <i class="fas fa-bolt"></i>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="card-body">
+                        <p class="card-text"><?= $item["description"] ?></p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Plus d'infos</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <!-- <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $item["title"] ?></h5>
+                        <p class="card-text"><?= $item["description"] ?></p>
+                        <a href="#" class="btn btn-primary">Plus d'infos</a>
+                    </div>
+                </div>
+            </div> -->
             
         <?php
         } 
